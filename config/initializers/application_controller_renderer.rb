096fea6397e04789582b22d0d238534a0d604954
NaoIkeji <na3o.g.7@gmail.com>
2019-09-07 11:38:43 +0000

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3bd2abb58452e0b5ae4a3af7ec6ea0e30e7c95c25c24df18b6767e4650f1d0e07aa147d75a595e7cba5cea6077030d63a69ef7a9fa3e935d5533a2ae88d67edd'
